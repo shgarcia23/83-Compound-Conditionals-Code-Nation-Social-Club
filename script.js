a31cc0dfc463c729d0a57fd7907f1e9eac708bfc
shgarcia23 <73551222+shgarcia23@users.noreply.github.com>
2021-06-04 13:07:28 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+// set the correct username and password combination below
+let correctUsername = "papabear21";
+let correctPassword = "1234";
+
+
+$("button").click(function() {
+    let username = $(".username").val();
+    let password = $(".password").val();
+    console.log(username);
+    console.log(password);
+    if(username===correctUsername){console.log("correct!");}
+     
+    
+    // WRITE YOUR COMPOUND CONDITIONAL BELOW THIS LINE
+
+      
+
+});
